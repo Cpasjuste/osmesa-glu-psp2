569424c4e1e57d43cabb064e720b2d1b64156a21
cpasjuste <cpasjuste@gmail.com>
2017-02-07 14:01:10 +0100

    psp2: first commit

---
+++ configure-psp2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CFLAGS="-Wl,-q -D__PSP2__ -O3 -mfpu=neon -mcpu=cortex-a9 -march=armv7-a -mfloat-abi=hard -ffast-math" \
+CXXFLAGS="$CFLAGS -fpermissive" \
+LIBS="-lpthread" \
+./configure --host=arm-vita-eabi --prefix=$VITASDK/arm-vita-eabi --disable-shared --enable-static --enable-osmesa
